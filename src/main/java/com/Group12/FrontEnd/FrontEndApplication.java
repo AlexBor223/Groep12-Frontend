aacb4cddb78d20ee96915877b8b59aef91d03893
RubenW556 <Ruubminer@gmail.com>
2021-10-19 18:43:32 +0200

    test

--- src/main/java/com/Group12/FrontEnd/FrontEndApplication.java
+++ src/main/java/com/Group12/FrontEnd/FrontEndApplication.java
@@ -2,12 +2,14 @@ package com.Group12.FrontEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javafx.application.Application;
 
 @SpringBootApplication
 public class FrontEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrontEndApplication.class, args);
+
+		Application.launch(WindowController.class, args);
 	}
 
 }
